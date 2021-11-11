7d9600df839b98000151fd13bce892d948190456
rounakpatel <78542032+rounakpatel@users.noreply.github.com>
2021-11-11 14:10:22 -0500

    Update Program (4).cs

--- Lab6/Program (4).cs
+++ Lab6/Program (4).cs
@@ -11,8 +11,8 @@ namespace Psim
 		static void Main(string[] args)
 		{
 			DispersionData dData;
-			dData.LaData = new double[] { -2.22e-7, 9260.0, 0.0};
-			dData.TaData = new double[] { -2.28e-7, 5240.0, 0.0};
+			dData.LaData = new double[] { -2.22e-7, 9260.0, 0.0 };
+			dData.TaData = new double[] { -2.28e-7, 5240.0, 0.0 };
 			dData.WMaxLa = 7.63916048e13;
 			dData.WMaxTa = 3.0100793072e13;
 
@@ -25,7 +25,22 @@ namespace Psim
 
 			Material silicon = new Material(in dData, in rData);
 
-			// Test the AddSensor, AddCell and SetSurfaces implementations. 
+			Model model = new Model(silicon, 800, 200, 20);
+
+			int numCells = 8;
+			for (int i = 0; i < numCells; ++i)
+			{
+
+				model.AddSensor(i, 450);
+				model.AddCell(8, 8, i);
+
+			}
+
+			model.SetSurfaces(500);
+			model.SetEmitPhonons(500, 10, 5e-9);
+
+			Console.WriteLine(model);
+
 		}
 	}
 }
